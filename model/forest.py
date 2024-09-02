1ea821a32f23dbf8d8625ade7aca6d6e42efbbab
dembo <a01709522@tec.mx>
2024-09-02 11:11:05 -0600

    add forest file

---
+++ model/forest.py
@@ -0,0 +1,12 @@
+# forest.py
+
+import numpy as np
+import pandas as pd
+
+"""
+@package docstring
+Este módulo contiene funciones para realizar predicciones con un modelo de random forest.
+
+@see https://en.wikipedia.org/wiki/Logistic_regression
+@see https://www.beyondphdcoaching.com/dissertation/binary-logistic-regression/
+"""
